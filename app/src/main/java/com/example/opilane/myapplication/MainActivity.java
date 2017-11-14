49c38749562f47359ce568dea9ae19229cba4930
Opilane TTHK <opilane@tthk.ee>
2017-11-14 08:55:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/opilane/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.opilane.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tekst;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tekst=(TextView)findViewById(R.id.test);
+
+    }
+
+
+
+}
